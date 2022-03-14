7c0ad1d8fe7a16d82c31862a1d88e60b56b3b108
Jared Nichols <Jararered@outlook.com>
2022-03-14 09:51:37 -0400

    altered order of initialization

--- source/comet/Engine.h
+++ source/comet/Engine.h
@@ -6,11 +6,13 @@
 #include "glad/gl.h"
 #include "glfw/glfw3.h"
 
-class MouseHandler;
-class KeyboardHandler;
-class Renderer;
 class WindowHandler;
 class EventHandler;
+class ErrorHandler;
+class MouseHandler;
+class KeyboardHandler;
+
+class Renderer;
 
 class Engine
 {
@@ -20,19 +22,21 @@ public:
 
     void Start();
     void Stop();
+    void MainLoop();
 
     std::shared_ptr<MouseHandler> GetMouseHandler() { return p_MouseHandler; }
     std::shared_ptr<KeyboardHandler> GetKeyboardHandler() { return p_KeyboardHandler; }
     std::shared_ptr<Renderer> GetRenderer() { return p_Renderer; }
     std::shared_ptr<WindowHandler> GetWindowHandler() { return p_WindowHandler; }
     std::shared_ptr<EventHandler> GetEventHandler() { return p_EventHandler; }
+    std::shared_ptr<ErrorHandler> GetErrorHandler() { return p_ErrorHandler; }
 
 private:
-    void MainLoop();
 
     std::shared_ptr<MouseHandler> p_MouseHandler;
     std::shared_ptr<KeyboardHandler> p_KeyboardHandler;
     std::shared_ptr<Renderer> p_Renderer;
     std::shared_ptr<WindowHandler> p_WindowHandler;
     std::shared_ptr<EventHandler> p_EventHandler;
+    std::shared_ptr<ErrorHandler> p_ErrorHandler;
 };
