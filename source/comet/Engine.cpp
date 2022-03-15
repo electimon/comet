217dae3d27bb0d38ee48d884b1091760dd3b3773
Jared Nichols <Jararered@outlook.com>
2022-03-15 09:17:30 -0400

    added camera, organized header file

--- source/comet/Engine.cpp
+++ source/comet/Engine.cpp
@@ -7,16 +7,19 @@
 #include "handlers/ErrorHandler.h"
 
 #include "render/Renderer.h"
+#include "render/Camera.h"
 
 Engine::Engine()
 {
+    m_TimeDelta = 0.0;
+
     p_WindowHandler = std::make_shared<WindowHandler>(this);
     p_EventHandler = std::make_shared<EventHandler>(this);
     p_KeyboardHandler = std::make_shared<KeyboardHandler>(this);
     p_MouseHandler = std::make_shared<MouseHandler>(this);
     p_ErrorHandler = std::make_shared<ErrorHandler>(this);
-
     p_Renderer = std::make_shared<Renderer>(this);
+    p_Camera = std::make_shared<Camera>(this);
 
     GetWindowHandler()->CreateWindow();
 }
@@ -33,7 +36,7 @@ void Engine::Start()
     p_ErrorHandler->SetupCallbacks();
 }
 
-void Engine::Stop()
+void Engine::Terminate()
 {
     glfwTerminate();
 }
@@ -42,15 +45,21 @@ void Engine::MainLoop()
 {
     while (!glfwWindowShouldClose(glfwGetCurrentContext()))
     {
+        // Clears color and depth buffers
         GetRenderer()->NewFrame();
 
-        // renderer should do things
+        // Update camera views for inputs
+        GetCamera()->Update();
+
+        // Drawing the mesh render queue
         GetRenderer()->DrawMeshQueue();
 
-        GetRenderer()->EndFrame();
+        // Swaps buffers to display new drawn frame
+        GetRenderer()->SwapBuffers();
 
+        // Poll events for next frame
         GetEventHandler()->PollEvents();
     }
 
-    Stop();
+    Terminate();
 }
