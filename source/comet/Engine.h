97439173e51120d737c3f9a2b03f399f789239e9
Jared Nichols <Jararered@outlook.com>
2022-03-15 09:17:41 -0400

    engine time tracking

--- source/comet/Engine.h
+++ source/comet/Engine.h
@@ -11,8 +11,8 @@ class EventHandler;
 class ErrorHandler;
 class MouseHandler;
 class KeyboardHandler;
-
 class Renderer;
+class Camera;
 
 class Engine
 {
@@ -21,22 +21,29 @@ public:
     ~Engine();
 
     void Start();
-    void Stop();
+    void Terminate();
     void MainLoop();
-
-    std::shared_ptr<MouseHandler> GetMouseHandler() { return p_MouseHandler; }
-    std::shared_ptr<KeyboardHandler> GetKeyboardHandler() { return p_KeyboardHandler; }
-    std::shared_ptr<Renderer> GetRenderer() { return p_Renderer; }
-    std::shared_ptr<WindowHandler> GetWindowHandler() { return p_WindowHandler; }
-    std::shared_ptr<EventHandler> GetEventHandler() { return p_EventHandler; }
-    std::shared_ptr<ErrorHandler> GetErrorHandler() { return p_ErrorHandler; }
+    double GetTimeDelta();
 
 private:
+    double m_TimeDelta;
 
+// Engine Components
+public:
+    std::shared_ptr<WindowHandler> GetWindowHandler() { return p_WindowHandler; }
+    std::shared_ptr<MouseHandler> GetMouseHandler() { return p_MouseHandler; }
+    std::shared_ptr<KeyboardHandler> GetKeyboardHandler() { return p_KeyboardHandler; }
+    std::shared_ptr<EventHandler> GetEventHandler() { return p_EventHandler; }
+    std::shared_ptr<ErrorHandler> GetErrorHandler() { return p_ErrorHandler; }
+    std::shared_ptr<Renderer> GetRenderer() { return p_Renderer; }
+    std::shared_ptr<Camera> GetCamera() { return p_Camera; }
+
+private:
+    std::shared_ptr<WindowHandler> p_WindowHandler;
     std::shared_ptr<MouseHandler> p_MouseHandler;
     std::shared_ptr<KeyboardHandler> p_KeyboardHandler;
-    std::shared_ptr<Renderer> p_Renderer;
-    std::shared_ptr<WindowHandler> p_WindowHandler;
     std::shared_ptr<EventHandler> p_EventHandler;
     std::shared_ptr<ErrorHandler> p_ErrorHandler;
+    std::shared_ptr<Renderer> p_Renderer;
+    std::shared_ptr<Camera> p_Camera;
 };
