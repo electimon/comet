ccd3f2a95c8460c921d5813384209b7ea2753970
Jared Nichols <Jararered@outlook.com>
2022-03-15 09:19:03 -0400

    test square

--- game/Main.cpp
+++ game/Main.cpp
@@ -3,37 +3,38 @@
 
 #include "render/containers/Shader.h"
 
-//test
-
 int main(void)
 {
-    Engine* p_Engine = new Engine;
+    Engine *p_Engine = new Engine;
     p_Engine->Start();
 
     std::vector<Vertex> vertices;
-    std::vector<unsigned int> indices;
+    std::vector<unsigned int> indices = {0, 1, 2, 2, 3, 0};
 
-    Vertex v1, v2, v3;
-    v1.position = {-0.5f, -0.5f, 0.0f};
-    v1.color = {1.0f, 0.0f, 0.0f};
-    v2.position = {0.5f, -0.5f, 0.0f};
-    v2.color = {0.0f, 1.0f, 0.0f};
-    v3.position = {0.0f, 0.5f, 0.0f};
-    v3.color = {0.0f, 0.0f, 1.0f};
-    indices.push_back(0);
-    indices.push_back(1);
-    indices.push_back(2);
-    vertices.push_back(v1);
-    vertices.push_back(v2);
-    vertices.push_back(v3);
+    std::vector<Vertex> v;
+    v.resize(4);
+    v[0].position = {-0.5f, -0.5f, 0.0f};
+    v[1].position = {0.5f, -0.5f, 0.0f};
+    v[2].position = {0.5f, 0.5f, 0.0f};
+    v[3].position = {-0.5f, 0.5f, 0.0f};
 
-    Shader basic;
-    basic.CompileShaders("../source/render/shaders/basic.vert", "../source/render/shaders/basic.frag");
+    vertices.push_back(v[0]);
+    vertices.push_back(v[1]);
+    vertices.push_back(v[2]);
+    vertices.push_back(v[3]);
 
-    Mesh triangle(vertices, indices);
-    triangle.AttachShader(basic.GetID());
+    glEnable(GL_CULL_FACE);
+    glCullFace(GL_BACK);
 
-    p_Engine->GetRenderer()->AddToMeshQueue(&triangle);
+    char vertexshader[] = "../source/render/shaders/basic.vert";
+    char fragshader[] = "../source/render/shaders/basic.frag";
+
+    Mesh square(vertices, indices);
+    Shader basic(vertexshader, fragshader);
+
+    square.SetShaderID(basic.GetID());
+
+    p_Engine->GetRenderer()->AddToMeshQueue(&square);
 
     p_Engine->MainLoop();
 
