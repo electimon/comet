86ef6609661f0f520a325f8e4875e6e0c072a0da
Jared Nichols <Jararered@outlook.com>
2022-03-17 12:48:28 -0400

    turned off debuggin

--- game/world/Block.h
+++ game/world/Block.h
@@ -8,12 +8,12 @@ public:
     Block(unsigned int id)
         : m_ID(id)
     {
-        std::cout << "Block::Block()" << std::endl;
+        // std::cout << "Block::Block()" << std::endl;
     }
 
     ~Block()
     {
-        std::cout << "Block::~Block()" << std::endl;
+        // std::cout << "Block::~Block()" << std::endl;
     }
 
 private:
