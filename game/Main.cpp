aa26dd3d8861422124de4e096ece467543138b61
Jared Nichols <Jararered@outlook.com>
2022-03-18 09:36:02 -0400

    moving onto 3d rendering

--- game/Main.cpp
+++ game/Main.cpp
@@ -13,29 +13,30 @@ int main(void)
 {
     Engine::Start();
 
+    char vertexshader[] = "../source/render/shaders/basic.vert";
+    char fragshader[] = "../source/render/shaders/basic.frag";
+    Shader basic(vertexshader, fragshader);
+
     World world;
     { // Generating a chunk
         double startTime = glfwGetTime();
         world.GenerateChunk(glm::ivec3(0, 0, 0));
-        double timeTaken = glfwGetTime() - startTime;
-        std::cout << "Chunk generated in: " << timeTaken << " seconds." << std::endl;
+        world.AddChunkToRenderQueue(glm::ivec3(0, 0, 0));
+        world.AddShaderToChunk(glm::ivec3(0, 0, 0), basic);
+        // std::cout << "Chunk generated in: " << glfwGetTime() - startTime << " seconds." << std::endl;
     }
 
     // Square for testing
-    std::vector<Vertex> vertices;
-    std::vector<unsigned int> indices = {0, 1, 2, 2, 3, 0};
-    vertices.resize(4);
-    vertices[0].position = {5.0f, 0.5f, -0.5f};
-    vertices[1].position = {5.0f, -0.5f, -0.5f};
-    vertices[2].position = {5.0f, -0.5f, 0.5f};
-    vertices[3].position = {5.0f, 0.5f, 0.5f};
-    char vertexshader[] = "../source/render/shaders/basic.vert";
-    char fragshader[] = "../source/render/shaders/basic.frag";
-    Mesh square(vertices, indices);
-    Shader basic(vertexshader, fragshader);
-    square.SetShaderID(basic.GetID());
-    Renderer::GetInstance().AddToMeshQueue(&square);
-
+    // std::vector<Vertex> vertices;
+    // std::vector<unsigned int> indices = {0, 1, 2, 2, 3, 0};
+    // vertices.resize(4);
+    // vertices[0].position = {5.0f, 0.5f, -0.5f};
+    // vertices[1].position = {5.0f, -0.5f, -0.5f};
+    // vertices[2].position = {5.0f, -0.5f, 0.5f};
+    // vertices[3].position = {5.0f, 0.5f, 0.5f};
+    // Mesh square(vertices, indices);
+    // square.SetShaderID(basic.GetID());
+    // Renderer::AddToMeshQueue(&square);
 
     Engine::MainLoop();
 
