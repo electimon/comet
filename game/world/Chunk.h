4ac7815895f67b23cd811d8ad851ff28b8f3a42e
Jared Nichols <Jararered@outlook.com>
2022-03-21 11:40:08 -0400

    tweaked how the fill chunk function works

--- game/world/Chunk.h
+++ game/world/Chunk.h
@@ -23,7 +23,7 @@ struct Chunk
     {
         std::cout << "Chunk::Chunk()" << std::endl;
         m_Blocks.reserve(m_ChunkSize * m_ChunkSize * m_ChunkHeight);
-        FillChunk(Block(1));
+        FillChunk();
         GenerateMesh();
         // p_World->p_Engine->GetRenderer()->AddToMeshQueue(&m_Mesh);
     }
@@ -38,7 +38,7 @@ struct Chunk
         m_Blocks.insert_or_assign(coordinate, block);
     }
 
-    void FillChunk(Block &block)
+    void FillChunk()
     {
         for (int i = 0; i < m_ChunkSize; i++)
         {
@@ -46,7 +46,13 @@ struct Chunk
             {
                 for (int k = 0; k < m_ChunkSize; k++)
                 {
-                    SetBlock(glm::ivec3(i, j, k), block);
+                    int blockID = 1;
+                    // int blockID = glm::linearRand(0, 1);
+                    if (blockID == 0)
+                    {
+                        continue;
+                    }
+                    SetBlock(glm::ivec3(i, j, k), Block(blockID));
                 }
             }
         }
@@ -65,9 +71,8 @@ struct Chunk
         float green = glm::linearRand(0.0f, 1.0f);
         float blue = glm::linearRand(0.0f, 1.0f);
 
-        float xOffset = float(m_Chunk.x * m_ChunkSize);
-        float zOffset = float(m_Chunk.z * m_ChunkSize);
-        // float yOffset = m_Chunk.y;
+        int xOffset = m_Chunk.x * m_ChunkSize;
+        int zOffset = m_Chunk.z * m_ChunkSize;
 
         for (int xRelPos = 0; xRelPos < m_ChunkSize; xRelPos++)
         {
@@ -75,9 +80,39 @@ struct Chunk
             {
                 for (int zRelPos = 0; zRelPos < m_ChunkSize; zRelPos++)
                 {
-                    float xPos = static_cast<float>(xRelPos) + xOffset;
-                    float yPos = static_cast<float>(yRelPos);
-                    float zPos = static_cast<float>(zRelPos) + zOffset;
+                    int xPos = xRelPos + xOffset;
+                    int yPos = yRelPos;
+                    int zPos = zRelPos + zOffset;
+
+                    if (m_Blocks.find(glm::ivec3(xRelPos, yRelPos, zRelPos)) == m_Blocks.end())
+                    {
+                        continue;
+                    }
+
+                    if (m_Blocks.find(glm::ivec3(xRelPos, yRelPos, zRelPos + 1)) == m_Blocks.end())
+                    {
+                        m_Indices.insert(m_Indices.end(), {0 + offset, 1 + offset, 2 + offset, 2 + offset, 3 + offset, 0 + offset}); // front
+                    }
+                    if (m_Blocks.find(glm::ivec3(xRelPos + 1, yRelPos, zRelPos)) == m_Blocks.end())
+                    {
+                        m_Indices.insert(m_Indices.end(), {1 + offset, 5 + offset, 6 + offset, 6 + offset, 2 + offset, 1 + offset}); // right
+                    }
+                    if (m_Blocks.find(glm::ivec3(xRelPos, yRelPos, zRelPos - 1)) == m_Blocks.end())
+                    {
+                        m_Indices.insert(m_Indices.end(), {5 + offset, 4 + offset, 7 + offset, 7 + offset, 6 + offset, 5 + offset}); // back
+                    }
+                    if (m_Blocks.find(glm::ivec3(xRelPos - 1, yRelPos, zRelPos)) == m_Blocks.end())
+                    {
+                        m_Indices.insert(m_Indices.end(), {4 + offset, 0 + offset, 3 + offset, 3 + offset, 7 + offset, 4 + offset}); // left
+                    }
+                    if (m_Blocks.find(glm::ivec3(xRelPos, yRelPos - 1, zRelPos)) == m_Blocks.end())
+                    {
+                        m_Indices.insert(m_Indices.end(), {0 + offset, 4 + offset, 5 + offset, 5 + offset, 1 + offset, 0 + offset}); // bottom
+                    }
+                    if (m_Blocks.find(glm::ivec3(xRelPos, yRelPos + 1, zRelPos)) == m_Blocks.end())
+                    {
+                        m_Indices.insert(m_Indices.end(), {3 + offset, 2 + offset, 6 + offset, 6 + offset, 7 + offset, 3 + offset}); // top
+                    }
 
                     m_Vertices.insert(m_Vertices.end(), {Vertex(xPos - 0.5f, yPos - 0.5f, zPos + 0.5f, red, green, blue),
                                                          Vertex(xPos + 0.5f, yPos - 0.5f, zPos + 0.5f, red, green, blue),
@@ -88,12 +123,6 @@ struct Chunk
                                                          Vertex(xPos + 0.5f, yPos + 0.5f, zPos - 0.5f, red, green, blue),
                                                          Vertex(xPos - 0.5f, yPos + 0.5f, zPos - 0.5f, red, green, blue)});
 
-                    m_Indices.insert(m_Indices.end(), {0 + offset, 1 + offset, 2 + offset, 2 + offset, 3 + offset, 0 + offset,   // front
-                                                       1 + offset, 5 + offset, 6 + offset, 6 + offset, 2 + offset, 1 + offset,   // right
-                                                       5 + offset, 4 + offset, 7 + offset, 7 + offset, 6 + offset, 5 + offset,   // back
-                                                       4 + offset, 0 + offset, 3 + offset, 3 + offset, 7 + offset, 4 + offset,   // left
-                                                       0 + offset, 4 + offset, 5 + offset, 5 + offset, 1 + offset, 0 + offset,   // bottom
-                                                       3 + offset, 2 + offset, 6 + offset, 6 + offset, 7 + offset, 3 + offset}); // top
                     offset += 8;
                 }
             }
@@ -101,7 +130,7 @@ struct Chunk
         m_Mesh.AddGemoetry(m_Vertices, m_Indices);
     }
 
-    void OptimizeMesh()
+    void Redraw()
     {
     }
 
