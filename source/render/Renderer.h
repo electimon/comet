6a534eb309d28e3443237d5a9c5555fbcd383443
Jared Nichols <Jararered@outlook.com>
2022-03-21 11:42:07 -0400

    added method of removing meshes from the queue

--- source/render/Renderer.h
+++ source/render/Renderer.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "glm/gtx/hash.hpp"
+
 #include "render/containers/Mesh.h"
 #include "render/containers/Shader.h"
 
@@ -15,15 +17,37 @@ public:
     static void Initialize();
     static void NewFrame();
     static void SwapBuffers();
-    static void DrawMeshQueue() { GetInstance().DrawMeshQueueFunction(); }
-    static void AddToMeshQueue(Mesh *mesh) { GetInstance().m_MeshQueue.push_back(mesh); }
+    static void DrawMeshes() { GetInstance().DrawMeshesFunction(); }
+    static void AddMesh(glm::ivec3 index, Mesh* mesh)
+    {
+        GetInstance().m_MeshMap.insert_or_assign(index, mesh);
+    }
+
+    static void RemoveMesh(int x, int y, int z)
+    {
+        RemoveMesh(glm::ivec3(x, y, z));
+    }
+
+    static void RemoveMesh(glm::ivec3 index)
+    {
+        if (GetInstance().m_MeshMap.find(index) != GetInstance().m_MeshMap.end())
+        {
+            GetInstance().m_MeshMap.at(index)->~Mesh();
+            GetInstance().m_MeshMap.erase(index);
+        }
+        else
+        {
+            std::cout << "Attmpted to remove mesh from queue that did not exist." << std::endl;
+        }
+    }
 
 private:
     Renderer() {}
     Renderer(Renderer const &);
     void operator=(Renderer const &);
 
-    void DrawMeshQueueFunction();
+    void DrawMeshesFunction();
 
-    std::vector<Mesh *> m_MeshQueue;
+    // std::vector<Mesh *> m_MeshQueue;
+    std::unordered_map<glm::ivec3, Mesh *> m_MeshMap;
 };
