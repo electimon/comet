38803c069f3c88d14388f0c993f6cbaf14510219
Jared Nichols <Jararered@outlook.com>
2022-03-21 06:47:39 -0400

    generate chunks on the heap now

--- game/world/World.h
+++ game/world/World.h
@@ -25,23 +25,38 @@ public:
         std::cout << "World::~World()" << std::endl;
     }
 
+    void GenerateChunk(int x, int y, int z)
+    {
+        GenerateChunk(glm::ivec3(x, y, z));
+    }
+
     void GenerateChunk(glm::ivec3 index)
     {
-        m_Chunks.insert_or_assign(index, Chunk(index));
+        Chunk *chunk = new Chunk(index);
+        m_Chunks.insert_or_assign(index, chunk);
+    }
+
+    void AddChunkToRenderQueue(int x, int y, int z)
+    {
+        AddChunkToRenderQueue(glm::ivec3(x, y, z));
     }
 
     void AddChunkToRenderQueue(glm::ivec3 index)
     {
-        Renderer::AddToMeshQueue(m_Chunks.at(index).GetMesh());
+        Renderer::AddToMeshQueue(m_Chunks.at(index)->GetMesh());
     }
 
     void AddShaderToChunk(glm::ivec3 index, Shader shader)
     {
-        m_Chunks.at(index).GetMesh()->SetShaderID(shader.GetID());
+        m_Chunks.at(index)->GetMesh()->SetShaderID(shader.GetID());
+    }
+    void AddShaderToChunk(int x, int y, int z, Shader &shader)
+    {
+        m_Chunks.at(glm::ivec3(x, y, z))->GetMesh()->SetShaderID(shader.GetID());
     }
 
 private:
     // This will be a temporary cache of the loaded chunks.
     // Functionallity to check for saved data on disk will eventually be implemented.
-    std::unordered_map<glm::ivec3, Chunk> m_Chunks;
+    std::unordered_map<glm::ivec3, Chunk *> m_Chunks;
 };
