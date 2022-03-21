fb8d990538ceb351657e91386032aeebe129570f
Jared Nichols <Jararered@outlook.com>
2022-03-21 06:47:48 -0400

    reintroduce colors

--- source/render/containers/Vertex.h
+++ source/render/containers/Vertex.h
@@ -7,10 +7,16 @@ struct Vertex
     Vertex()
     {
     }
+    Vertex(float xPos, float yPos, float zPos, float r, float g, float b)
+        : position(xPos, yPos, zPos),
+          color(r, g, b)
+    {
+    }
     Vertex(float xPos, float yPos, float zPos)
-        : position(xPos, yPos, zPos)
+        : position(xPos, yPos, zPos),
+          color(1.0f, 1.0f, 1.0f)
     {
     }
     glm::vec3 position;
-    // glm::vec3 color;
+    glm::vec3 color;
 };
