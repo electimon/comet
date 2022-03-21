f99e1dd1df592eb5da1394b51c25f885f18a546b
Jared Nichols <Jararered@outlook.com>
2022-03-21 08:16:45 -0400

    removed test cube, testing generating arrays of chunks

--- game/Main.cpp
+++ game/Main.cpp
@@ -7,36 +7,25 @@
 #include "world/Chunk.h"
 #include "world/Block.h"
 
-#include "glfw/glfw3.h"
-
 int main(void)
 {
-    Engine::Start();
+    Engine::Initialize();
 
     char vertexshader[] = "../source/render/shaders/basic.vert";
     char fragshader[] = "../source/render/shaders/basic.frag";
     Shader basic(vertexshader, fragshader);
 
     World world;
-    { // Generating a chunk
-        double startTime = glfwGetTime();
-        world.GenerateChunk(0, 0, 0);
-        world.AddChunkToRenderQueue(0, 0, 0);
-        world.AddShaderToChunk(0, 0, 0, basic);
-        std::cout << "Chunk generated in: " << glfwGetTime() - startTime << " seconds." << std::endl;
+    for (int x = -4; x < 4; x++)
+    {
+        for (int z = -4; z < 4; z++)
+        { // Generating a chunk
+            Timer timer("chunk generation");
+            world.GenerateChunk(x, 0, z);
+            world.AddChunkToRenderQueue(x, 0, z);
+            world.AddShaderToChunk(x, 0, z, basic);
+        }
     }
-
-    // Square for testing
-    // std::vector<Vertex> vertices;
-    // std::vector<unsigned int> indices = {0, 1, 2, 2, 3, 0};
-    // vertices.resize(4);
-    // vertices[0].position = {5.0f, 0.5f, -0.5f};
-    // vertices[1].position = {5.0f, -0.5f, -0.5f};
-    // vertices[2].position = {5.0f, -0.5f, 0.5f};
-    // vertices[3].position = {5.0f, 0.5f, 0.5f};
-    // Mesh square(vertices, indices);
-    // square.SetShaderID(basic.GetID());
-    // Renderer::AddToMeshQueue(&square);
 
     Engine::MainLoop();
 
