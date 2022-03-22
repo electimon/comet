5a2a5d7a153efa23ceddc90117f1367130cf3375
Jared Nichols <Jararered@outlook.com>
2022-03-22 08:42:52 -0400

    chunk request functions

--- game/entities/Player.cpp
+++ game/entities/Player.cpp
@@ -1,8 +1,13 @@
 #include "Player.h"
 
+#include <thread>
+
 Player::Player()
 {
     EntityHandler::AddEntityToQueue(this);
+
+    m_RenderDistance = 4;
+    m_ChunkSize = World::GetChunkSize();
 }
 
 Player::~Player()
@@ -11,6 +16,52 @@ Player::~Player()
 
 void Player::Update()
 {
-    std::cout << "Player updated." << std::endl;
     m_Position = Camera::GetPosition();
+
+    glm::ivec3 newChunkIndex;
+
+    if (m_Position.x < 0.0f)
+    {
+        newChunkIndex.x = ((float)m_Position.x - m_ChunkSize) / m_ChunkSize;
+    }
+    else
+    {
+        newChunkIndex.x = ((float)m_Position.x) / m_ChunkSize;
+    }
+
+    if (m_Position.z < 0.0f)
+    {
+        newChunkIndex.z = ((float)m_Position.z - m_ChunkSize) / m_ChunkSize;
+    }
+    else
+    {
+        newChunkIndex.z = ((float)m_Position.z) / m_ChunkSize;
+    }
+
+    if (newChunkIndex != m_ChunkIndex)
+    {
+        // Update requested chunks if the chunk index changes
+        std::cout << "Entered chunk: " << newChunkIndex.x << " " << newChunkIndex.z << std::endl;
+
+        m_ChunkIndex = newChunkIndex;
+        UpdateRequestedChunks();
+    }
+}
+
+void Player::UpdateRequestedChunks()
+{
+    // Begin new set of chunks to request
+    m_RequestedChunks.clear();
+
+    // Loop through square shape around the player
+    for (int x = m_ChunkIndex.x - m_RenderDistance; x < 1 + m_ChunkIndex.x + m_RenderDistance; x++)
+    {
+        for (int z = m_ChunkIndex.z - m_RenderDistance; z < 1 + m_ChunkIndex.z + m_RenderDistance; z++)
+        {
+            m_RequestedChunks.insert(glm::ivec3(x, 0, z));
+        }
+    }
+
+    // Pass set of new chunks to the world to deal with
+    p_World->ProcessRequestedChunks(m_RequestedChunks);
 }
