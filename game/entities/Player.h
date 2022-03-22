2402b8f0fc3483273ce71d5e5635556f9de75940
Jared Nichols <Jararered@outlook.com>
2022-03-22 08:44:12 -0400

    local saved variables

--- game/entities/Player.h
+++ game/entities/Player.h
@@ -1,11 +1,15 @@
 #pragma once
 
 #include <iostream>
+#include <unordered_set>
 
 #include "glm/glm.hpp"
 
 #include "handlers/Entity.h"
 #include "handlers/EntityHandler.h"
+
+#include "world/Chunk.h"
+#include "world/World.h"
 
 class Player : public Entity
 {
@@ -15,8 +19,17 @@ public:
 
     void Update();
 
+    glm::vec3 GetPosition() { return m_Position; }
+    void UpdateRequestedChunks();
+
+    void SetWorld(World *world) { p_World = world; }
+
 private:
     glm::vec3 m_Position;
     glm::ivec3 m_ChunkIndex;
+    std::unordered_set<glm::ivec3> m_RequestedChunks;
+    int m_RenderDistance;
+    int m_ChunkSize;
 
+    World *p_World;
 };
