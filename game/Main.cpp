63ead8393229863f0d7986a08f3834fc871eaaac
Jared Nichols <Jararered@outlook.com>
2022-03-22 10:28:23 -0400

    removed timer and commented chunks

--- game/Main.cpp
+++ game/Main.cpp
@@ -17,19 +17,8 @@ int main(void)
     char fragshader[] = "../source/render/shaders/basic.frag";
     Shader basic(vertexshader, fragshader);
 
-    Timer timer("chunk generation");
 
     World world;
-    // for (int x = -1; x < 1; x++)
-    // {
-    //     for (int z = -1; z < 1; z++)
-    //     { // Generating a chunk
-    //         world.GenerateChunk(x, 0, z);
-    //         world.AddChunkToRenderQueue(x, 0, z);
-    //         world.AddShaderToChunk(x, 0, z, basic.GetID());
-    //     }
-    // }
-
     Player player;
     player.SetWorld(&world);
 
