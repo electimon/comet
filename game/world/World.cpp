edde0bace52ee260ba1337f9f205e1eaf73b8aeb
Jared Nichols <Jararered@outlook.com>
2022-03-22 08:43:28 -0400

    process chunk requests function

--- game/world/World.cpp
+++ game/world/World.cpp
@@ -12,7 +12,7 @@ World::~World()
 void World::GenerateChunk(glm::ivec3 index)
 {
     Chunk *chunk = new Chunk(index);
-    m_Chunks.insert_or_assign(index, chunk);
+    m_ChunkMap.insert_or_assign(index, chunk);
 }
 void World::GenerateChunk(int x, int y, int z)
 {
@@ -21,7 +21,7 @@ void World::GenerateChunk(int x, int y, int z)
 
 void World::AddChunkToRenderQueue(glm::ivec3 index)
 {
-    Renderer::AddMesh(index, m_Chunks.at(index)->GetMesh());
+    Renderer::AddMesh(index, m_ChunkMap.at(index)->GetMesh());
 }
 void World::AddChunkToRenderQueue(int x, int y, int z)
 {
@@ -30,9 +30,47 @@ void World::AddChunkToRenderQueue(int x, int y, int z)
 
 void World::AddShaderToChunk(glm::ivec3 index, unsigned int shader)
 {
-    m_Chunks.at(index)->GetMesh()->SetShaderID(shader);
+    m_ChunkMap.at(index)->GetMesh()->SetShaderID(shader);
 }
 void World::AddShaderToChunk(int x, int y, int z, unsigned int shader)
 {
     AddShaderToChunk(glm::ivec3(x, y, z), shader);
+}
+
+void World::ProcessRequestedChunks(std::unordered_set<glm::ivec3> requestedChunkIndices)
+{
+    std::unordered_set<glm::ivec3> chunksToRemove;
+
+    // Loops through the currently loaded chunks and checks if they are
+    // present in the requested chunks. If not, added to the chunksToRemove set.
+    // Removes from the rendering mesh queue as well. Removing from the map
+    // while looping over it is bad.
+    for (const auto &oldChunk : m_ChunkMap)
+    {
+        if (requestedChunkIndices.find(oldChunk.first) == requestedChunkIndices.end())
+        {
+            // old chunk not found in requested chunks, remove remove queue and map
+            Renderer::RemoveMesh(oldChunk.first);
+            chunksToRemove.insert(oldChunk.first);
+        }
+    }
+
+    // Loops through the set of chunksToRemove set and removes them from the
+    // currently loaded chunks in the world
+    for (const auto & chunk : chunksToRemove)
+    {
+        m_ChunkMap.erase(chunk);
+    }
+
+
+    for (const auto &newChunkIndex : requestedChunkIndices)
+    {
+        if (m_ChunkMap.find(newChunkIndex) == m_ChunkMap.end())
+        {
+            // new chunk not found in old chunks, add to render queue
+            GenerateChunk(newChunkIndex);
+            AddChunkToRenderQueue(newChunkIndex);
+            AddShaderToChunk(newChunkIndex, 1); // TODO: add shader properly
+        }
+    }
 }
--- game/world/World.h
+++ game/world/World.h
@@ -1,23 +1,24 @@
 #pragma once
 
 #include <unordered_map>
+#include <unordered_set>
+
+#include "glm/glm.hpp"
 
 #include "glm/gtx/hash.hpp"
 
-#include "render/Renderer.h"
+#include "Renderer.h"
 
 #include "Chunk.h"
-
-/*
-index = used to describe chunk locations
-coordinate = used to describe block locations
-*/
 
 class World
 {
 public:
     World();
     ~World();
+
+    static int GetChunkSize() { return 16; }
+    static int GetChunkHeight() { return 8; }
 
     void GenerateChunk(glm::ivec3 index);
     void GenerateChunk(int x, int y, int z);
@@ -28,8 +29,10 @@ public:
     void AddShaderToChunk(glm::ivec3 index, unsigned int shader);
     void AddShaderToChunk(int x, int y, int z, unsigned int shader);
 
+    void ProcessRequestedChunks(std::unordered_set<glm::ivec3> chunks);
+
 private:
     // This will be a temporary cache of the loaded chunks.
     // Functionallity to check for saved data on disk will eventually be implemented.
-    std::unordered_map<glm::ivec3, Chunk *> m_Chunks;
+    std::unordered_map<glm::ivec3, Chunk *> m_ChunkMap;
 };
