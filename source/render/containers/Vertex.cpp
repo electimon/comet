021b02c059be18e8826849bd8bcc9c89adc9943d
Jared Nichols <Jararered@outlook.com>
2022-03-22 05:29:31 -0400

    separate declarations and definitions

---
+++ source/render/containers/Vertex.cpp
@@ -0,0 +1,17 @@
+#include "Vertex.h"
+
+Vertex::Vertex()
+{
+}
+
+Vertex::Vertex(float xPos, float yPos, float zPos)
+    : position(xPos, yPos, zPos),
+      color(1.0f, 1.0f, 1.0f)
+{
+}
+
+Vertex::Vertex(float xPos, float yPos, float zPos, float r, float g, float b)
+    : position(xPos, yPos, zPos),
+      color(r, g, b)
+{
+}
--- source/render/containers/Vertex.h
+++ source/render/containers/Vertex.h
@@ -4,21 +4,9 @@
 
 struct Vertex
 {
-    Vertex()
-    {
-    }
-
-    Vertex(float xPos, float yPos, float zPos, float r, float g, float b)
-        : position(xPos, yPos, zPos),
-          color(r, g, b)
-    {
-    }
-
-    Vertex(float xPos, float yPos, float zPos)
-        : position(xPos, yPos, zPos),
-          color(1.0f, 1.0f, 1.0f)
-    {
-    }
+    Vertex();
+    Vertex(float xPos, float yPos, float zPos);
+    Vertex(float xPos, float yPos, float zPos, float r, float g, float b);
 
     glm::vec3 position;
     glm::vec3 color;
