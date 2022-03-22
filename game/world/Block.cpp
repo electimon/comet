92461c338d69a2ce9fcf82627faa7c51f04270a4
Jared Nichols <Jararered@outlook.com>
2022-03-22 08:43:51 -0400

    comment out constructor/destructor output

--- game/world/Block.cpp
+++ game/world/Block.cpp
@@ -3,12 +3,12 @@
 Block::Block(unsigned int id)
     : m_ID(id)
 {
-    std::cout << "Block::Block()" << std::endl;
+    // std::cout << "Block::Block()" << std::endl;
 }
 
 Block::~Block()
 {
-    std::cout << "Block::~Block()" << std::endl;
+    // std::cout << "Block::~Block()" << std::endl;
 }
 
 int Block::GetID()
