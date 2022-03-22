1da6a6173fc0c0d22aecee37f17fe100a4119478
Jared Nichols <Jararered@outlook.com>
2022-03-22 12:30:59 -0400

    added constructors using glm vectors

--- source/render/containers/Vertex.cpp
+++ source/render/containers/Vertex.cpp
@@ -15,8 +15,21 @@ Vertex::Vertex(float xPos, float yPos, float zPos, float r, float g, float b)
       color(r, g, b, 1.0f)
 {
 }
+
 Vertex::Vertex(float xPos, float yPos, float zPos, float r, float g, float b, float a)
     : position(xPos, yPos, zPos),
       color(r, g, b, a)
+{
+}
+
+Vertex::Vertex(float xPos, float yPos, float zPos, glm::vec3 rgb)
+    : position(xPos, yPos, zPos),
+      color(rgb, 1.0f)
+{
+}
+
+Vertex::Vertex(float xPos, float yPos, float zPos, glm::vec4 rgba)
+    : position(xPos, yPos, zPos),
+      color(rgba)
 {
 }
