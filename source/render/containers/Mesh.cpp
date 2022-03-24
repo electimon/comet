e9a0c645e2b321e076822391d643ce1e4180fb79
Jared Nichols <Jararered@outlook.com>
2022-03-24 05:41:16 -0400

    sent wrong data to gpu

--- source/render/containers/Mesh.cpp
+++ source/render/containers/Mesh.cpp
@@ -46,9 +46,9 @@ void Mesh::PushToGPU()
 
     glBindVertexArray(m_VAO);
     glBindBuffer(GL_ARRAY_BUFFER, m_VBO);
-    glBufferData(GL_ARRAY_BUFFER, p_Vertices->size() * sizeof(Vertex), &p_Vertices[0], GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, p_Vertices->size() * sizeof(Vertex), p_Vertices->data(), GL_STATIC_DRAW);
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_IBO);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, p_Indices->size() * sizeof(unsigned int), &p_Indices[0], GL_STATIC_DRAW);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, p_Indices->size() * sizeof(unsigned int), p_Indices->data(), GL_STATIC_DRAW);
 
     glEnableVertexAttribArray(0);
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void *)offsetof(Vertex, position));
