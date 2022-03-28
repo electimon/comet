84b0d2b350f85abd366c5c0c4b192c25b0216990
Jared Nichols <Jararered@outlook.com>
2022-03-28 11:00:00 -0400

    stored noise generation in separate class

--- game/world/ChunkGenerator.h
+++ game/world/ChunkGenerator.h
@@ -12,21 +12,45 @@ public:
     }
     static void Initialize()
     {
-        GetInstance().m_SurfaceNoise.SetNoiseType(FastNoiseLite::NoiseType::NoiseType_OpenSimplex2);
-        GetInstance().m_SurfaceNoise.SetFrequency(0.02f);
-        GetInstance().m_SurfaceNoise.SetSeed(1337);
+        GetInstance().m_ChunkHeight = 512;
+        GetInstance().m_ChunkSize = 16;
 
-        GetInstance().m_BiomeNoise.SetNoiseType(FastNoiseLite::NoiseType::NoiseType_OpenSimplex2);
-        GetInstance().m_BiomeNoise.SetFrequency(0.001f);
-        GetInstance().m_BiomeNoise.SetSeed(1337);
+        GetInstance().m_MediumNoise.SetNoiseType(FastNoiseLite::NoiseType::NoiseType_OpenSimplex2);
+        GetInstance().m_MediumNoise.SetFrequency(0.01f);
+        GetInstance().m_MediumNoise.SetSeed(1337);
+
+        GetInstance().m_SlowNoise.SetNoiseType(FastNoiseLite::NoiseType::NoiseType_OpenSimplex2);
+        GetInstance().m_SlowNoise.SetFrequency(0.001f);
+        GetInstance().m_SlowNoise.SetSeed(1337);
+
+        GetInstance().m_CellularNoise.SetNoiseType(FastNoiseLite::NoiseType::NoiseType_Cellular);
+        GetInstance().m_CellularNoise.SetFrequency(0.01f);
+        GetInstance().m_CellularNoise.SetSeed(1337);
+
+        GetInstance().m_FastNoise.SetNoiseType(FastNoiseLite::NoiseType::NoiseType_OpenSimplex2);
+        GetInstance().m_FastNoise.SetFrequency(1.0f);
+        GetInstance().m_FastNoise.SetSeed(1337);
     }
-    static float GetSurfaceNoise(int x, int z)
+    static float GetMediumNoise(int x, int z)
     {
-        return GetInstance().m_SurfaceNoise.GetNoise(static_cast<float>(x), static_cast<float>(z));
+        return GetInstance().m_MediumNoise.GetNoise(static_cast<float>(x), static_cast<float>(z));
     }
-    static float GetBiomeNoise(int x, int z)
+    static float GetSlowNoise(int x, int z)
     {
-        return GetInstance().m_BiomeNoise.GetNoise(static_cast<float>(x), static_cast<float>(z));
+        return GetInstance().m_SlowNoise.GetNoise(static_cast<float>(x), static_cast<float>(z));
+    }
+    static float GetCellularNoise(int x, int y)
+    {
+        return GetInstance().m_CellularNoise.GetNoise(static_cast<float>(x), static_cast<float>(y));
+    }
+    static float GetFastNoise(int x, int y, int z)
+    {
+        return GetInstance().m_FastNoise.GetNoise(static_cast<float>(x), static_cast<float>(y), static_cast<float>(z));
+    }
+
+    static void GenerateChunk(const glm::ivec3 &chunkIndex, const std::vector<unsigned char> *blockData)
+    {
+        // move all chunk generation into here eventually
     }
 
 private:
@@ -34,7 +58,12 @@ private:
     ChunkGenerator(ChunkGenerator const &);
     void operator=(ChunkGenerator const &);
 
-    FastNoiseLite m_SurfaceNoise;
-    FastNoiseLite m_BiomeNoise;
+    FastNoiseLite m_MediumNoise;
+    FastNoiseLite m_SlowNoise;
+    FastNoiseLite m_CellularNoise;
+    FastNoiseLite m_FastNoise;
 
+    glm::ivec3 m_ChunkIndex;
+    int m_ChunkHeight;
+    int m_ChunkSize;
 };
