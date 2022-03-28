9d1bb2627e5fa51a134c1f834774cc04122eebb0
Jared Nichols <Jararered@outlook.com>
2022-03-28 11:54:20 -0400

    more noise patterns

--- game/world/ChunkGenerator.h
+++ game/world/ChunkGenerator.h
@@ -30,6 +30,28 @@ public:
         GetInstance().m_FastNoise.SetNoiseType(FastNoiseLite::NoiseType::NoiseType_OpenSimplex2);
         GetInstance().m_FastNoise.SetFrequency(1.0f);
         GetInstance().m_FastNoise.SetSeed(1337);
+
+        GetInstance().m_MediumChaotic.SetNoiseType(FastNoiseLite::NoiseType::NoiseType_OpenSimplex2);
+        GetInstance().m_MediumChaotic.SetFractalType(FastNoiseLite::FractalType::FractalType_FBm);
+        GetInstance().m_MediumChaotic.SetFractalOctaves(4);
+        GetInstance().m_MediumChaotic.SetFractalLacunarity(2.00f);
+        // GetInstance().m_MediumChaotic.SetFractalGain(0.5f);
+        GetInstance().m_MediumChaotic.SetFrequency(0.01f);
+
+        GetInstance().m_BiomeNoise.SetNoiseType(FastNoiseLite::NoiseType::NoiseType_Value);
+        GetInstance().m_BiomeNoise.SetFrequency(0.015f);
+        GetInstance().m_BiomeNoise.SetCellularDistanceFunction(FastNoiseLite::CellularDistanceFunction::CellularDistanceFunction_Hybrid);
+        GetInstance().m_BiomeNoise.SetCellularReturnType(FastNoiseLite::CellularReturnType::CellularReturnType_CellValue);
+        GetInstance().m_BiomeNoise.SetDomainWarpType(FastNoiseLite::DomainWarpType::DomainWarpType_BasicGrid);
+        GetInstance().m_BiomeNoise.SetDomainWarpAmp(50.0f);
+    }
+    static float GetBiomeNoise(int x, int z)
+    {
+        return GetInstance().m_BiomeNoise.GetNoise(static_cast<float>(x), static_cast<float>(z));
+    }
+    static float GetMediumChaotic(int x, int z)
+    {
+        return GetInstance().m_MediumChaotic.GetNoise(static_cast<float>(x), static_cast<float>(z));
     }
     static float GetMediumNoise(int x, int z)
     {
@@ -43,9 +65,9 @@ public:
     {
         return GetInstance().m_CellularNoise.GetNoise(static_cast<float>(x), static_cast<float>(y));
     }
-    static float GetFastNoise(int x, int y, int z)
+    static float GetFastNoise(int x, int z)
     {
-        return GetInstance().m_FastNoise.GetNoise(static_cast<float>(x), static_cast<float>(y), static_cast<float>(z));
+        return GetInstance().m_FastNoise.GetNoise(static_cast<float>(x), static_cast<float>(z));
     }
 
     static void GenerateChunk(const glm::ivec3 &chunkIndex, const std::vector<unsigned char> *blockData)
@@ -62,6 +84,8 @@ private:
     FastNoiseLite m_SlowNoise;
     FastNoiseLite m_CellularNoise;
     FastNoiseLite m_FastNoise;
+    FastNoiseLite m_MediumChaotic;
+    FastNoiseLite m_BiomeNoise;
 
     glm::ivec3 m_ChunkIndex;
     int m_ChunkHeight;
