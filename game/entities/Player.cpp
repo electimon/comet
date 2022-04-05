66d0e20c9fd2e847afcbdac945745caac85040bc
Jared Nichols <Jararered@outlook.com>
2022-04-05 14:23:57 -0400

    higher render distance

--- game/entities/Player.cpp
+++ game/entities/Player.cpp
@@ -8,7 +8,7 @@ Player::Player(glm::vec3 position)
     Camera::SetPosition(position);
     EntityHandler::AddEntityToQueue(this);
 
-    m_RenderDistance = 2;
+    m_RenderDistance = 6;
 }
 
 Player::~Player()
