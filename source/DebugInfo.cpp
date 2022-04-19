61d1c88c9dd3cf9451b6b07697ff8f8e23cbc2dc
Jared Nichols <Jararered@outlook.com>
2022-04-19 07:33:58 -0400

    adjusted size

--- source/DebugInfo.cpp
+++ source/DebugInfo.cpp
@@ -19,11 +19,11 @@ void DebugInfo::DrawFrame()
         ImGui::Begin("Debug Info Panel");
 
         ImGui::SetWindowPos(ImVec2(0.0f, 0.0f));
-        ImGui::SetWindowSize(ImVec2(400.0f, 50.0f));
+        ImGui::SetWindowSize(ImVec2(400.0f, 100.0f));
 
         ImGui::Text("Application average: %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
 
-        ImGui::Text("Camera position: %.3f, %.3f, %.3f", Camera::GetPosition().x, Camera::GetPosition().y, Camera::GetPosition().z);
+        ImGui::Text("Camera position: %.3f, %.3f, %.3f", Camera::Position().x, Camera::Position().y, Camera::Position().z);
 
         ImGui::End();
     }
