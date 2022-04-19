327579e2ecdfeac145f9ab14b5e72e8203d86ac5
Jared Nichols <Jararered@outlook.com>
2022-04-19 07:31:13 -0400

    generated get/set funcs

--- source/Timer.h
+++ source/Timer.h
@@ -3,13 +3,20 @@
 
 #include <comet.pch>
 
-struct Timer
+class Timer
 {
     Timer();
     Timer(const std::string &scope);
     ~Timer();
 
-private:
+  private:
     double m_StartTime;
     std::string m_ScopeName;
+
+  public:
+    double StartTime() const { return m_StartTime; }
+    void SetStartTime(double StartTime) { m_StartTime = StartTime; }
+
+    std::string ScopeName() const { return m_ScopeName; }
+    void SetScopeName(const std::string &ScopeName) { m_ScopeName = ScopeName; }
 };
