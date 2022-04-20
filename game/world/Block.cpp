71f60b3803eb142440ffb8f155a62a548ecc717d
aerolust <Jararered@outlook.com>
2022-04-19 20:51:13 -0400

    added more transparent blocks

--- game/world/Block.cpp
+++ game/world/Block.cpp
@@ -2,7 +2,7 @@
 
 Block::Block() {}
 
-Block::Block(unsigned char id) : m_ID(id) { m_Transparent = (id == 0 || id == 4 || id == 6) ? true : false; }
+Block::Block(unsigned char id) : m_ID(id) { m_Transparent = (id == 0 || id == 4 || id == 6 || id == 18 || id == 20) ? true : false; }
 
 Block::Block(unsigned char id, bool transparent) : m_ID(id), m_Transparent(transparent) {}
 
