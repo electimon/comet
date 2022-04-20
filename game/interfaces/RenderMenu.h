40d7f6e34bba7e578d5b56b604b79e7f231dc0b9
Jared Nichols <Jararered@outlook.com>
2022-04-20 07:48:40 -0400

    game side menu creation

--- game/interfaces/RenderMenu.h
+++ game/interfaces/RenderMenu.h
@@ -5,6 +5,8 @@
 #include "handlers/Interface.h"
 #include "handlers/InterfaceHandler.h"
 
+#include "world/World.h"
+
 class RenderMenu : public Interface
 {
   public:
@@ -13,10 +15,54 @@ class RenderMenu : public Interface
 
     void Draw() override
     {
-        ImGui::Begin("Test");
-        ImGui::Button("Happy!");
+        ImGui::Begin("Render Menu");
+
+        if (ImGui::Button("Play"))
+        {
+            MouseHandler::CaptureCursor();
+        }
+        ImGui::SameLine();
+        if (ImGui::Button("Exit"))
+        {
+            Engine::SetShouldClose(true);
+        }
+
+        ImGui::Text("Render Modes: ");
+        ImGui::SameLine();
+        if (ImGui::Button("Wireframe"))
+        {
+            glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+        }
+        ImGui::SameLine();
+        if (ImGui::Button("Filled"))
+        {
+            glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+        }
+
+        if (ImGui::InputInt("Render Distance", &dummyvar1))
+        {
+            World::SetRenderDistance(dummyvar1);
+        }
+        if (ImGui::InputInt("World Seed", &dummyvar2))
+        {
+            ChunkGenerator::SetSeed(dummyvar2);
+        }
+
+        if (ImGui::SliderFloat3("Overlay Color", &dummyfloatvec1[0], -1.0f, 1.0f))
+        {
+            Renderer::SetOverlayColor(dummyfloatvec1);
+        }
+        if (ImGui::SliderFloat3("Background Color", &dummyfloatvec2[0], 0.0f, 1.0f))
+        {
+            Renderer::SetBackgroundColor(dummyfloatvec2);
+        }
+
         ImGui::End();
     }
 
   private:
+    int dummyvar1 = 4;
+    int dummyvar2 = 4;
+    glm::vec3 dummyfloatvec1 = {0.0f, 0.0f, 0.0f};
+    glm::vec3 dummyfloatvec2 = {0.0f, 0.0f, 0.0f};
 };
