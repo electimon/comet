01084f4a6cceee1137de7c0de37f03ecac0f992d
aerolust <Jararered@outlook.com>
2022-04-21 14:54:47 -0400

    solid flag

--- game/world/Block.cpp
+++ game/world/Block.cpp
@@ -2,8 +2,19 @@
 
 Block::Block() {}
 
-Block::Block(unsigned char id) : m_ID(id) { m_Transparent = (id == 0 || id == 4 || id == 6 || id == 18 || id == 20) ? true : false; }
+Block::Block(unsigned char id) : m_ID(id)
+{
+    m_Transparent = (id == 0 || id == 4 || id == 6 || id == 18 || id == 20) ? true : false;
+}
 
-Block::Block(unsigned char id, bool transparent) : m_ID(id), m_Transparent(transparent) {}
+Block::Block(unsigned char id, bool transparent) : m_ID(id), m_Transparent(transparent)
+{
+    //
+}
+Block::Block(unsigned char id, bool transparent, bool solid)
+    : m_ID(id), m_Transparent(transparent), m_Solid(solid)
+{
+    //
+}
 
 Block::~Block() {}
--- game/world/Block.h
+++ game/world/Block.h
@@ -6,17 +6,23 @@ class Block
     Block();
     Block(unsigned char id);
     Block(unsigned char id, bool transparent);
+    Block(unsigned char id, bool transparent, bool solid);
     ~Block();
-
-    unsigned char ID() const { return m_ID; }
-    void SetID(unsigned char ID) { m_ID = ID; }
-
-    bool IsTransparent() const { return m_Transparent; }
-    void SetTransparent(bool Transparent) { m_Transparent = Transparent; }
 
     template <class Archive> void serialize(Archive &ar) { ar(m_ID, m_Transparent); }
 
   private:
     unsigned char m_ID;
     bool m_Transparent;
+    bool m_Solid;
+
+  public:
+    unsigned char ID() const { return m_ID; }
+    void SetID(unsigned char ID) { m_ID = ID; }
+
+    bool IsTransparent() const { return m_Transparent; }
+    void SetTransparent(bool Transparent) { m_Transparent = Transparent; }
+
+    bool IsSolid() const { return m_Solid; }
+    void SetSolid(bool Solid) { m_Solid = Solid; }
 };
